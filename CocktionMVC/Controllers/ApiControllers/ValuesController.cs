dbca1ee20ae6354a23a6a96ca4e8d31278881513
sanalazar <lazarenko.ale@gmail.com>
2015-02-20 15:00:29 +0300

    Web Api Auth for Ios + refactoring
    Настроенная авторизация для мобильных клиентов + рефакторинг кода.

---
+++ CocktionMVC/Controllers/ApiControllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+namespace CocktionMVC.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "sdfsdf";
+        }
+
+        [Authorize]
+        [HttpPost]
+        public string Post([FromBody]Customer customer)
+        {
+            //Метод, который принимает ответ типа кастомер и 
+            //отправляет строчку
+            //данные сюда отправляются из вьюшки "Тест"
+            Customer cust = customer;
+            return "string";
+        }
+
+        
+        
+
+    }
+
+    public class Customer 
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+    }
+}
--- CocktionMVC/Global.asax.cs
+++ CocktionMVC/Global.asax.cs
@@ -9,19 +9,20 @@ using System.Data.Entity;
 using Microsoft.AspNet.Identity;
 using CocktionMVC.Models.DAL;
 using CocktionMVC.Models;
+using System.Web.Http;
 namespace CocktionMVC
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             Database.SetInitializer<CocktionContext>(new DropCreateDatabaseIfModelChanges<CocktionContext>());
             Database.SetInitializer<ApplicationDbContext>(new DropCreateDatabaseIfModelChanges<ApplicationDbContext>());
-
         }
     }
 }
