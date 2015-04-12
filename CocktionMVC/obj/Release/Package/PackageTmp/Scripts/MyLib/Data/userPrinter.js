ee2f9ec08256baf15e8bd8feac048d8ececb23a8
sanalazar <lazarenko.ale@gmail.com>
2015-04-12 13:28:24 +0300

    Обновления страниц Дом, Люди

--- CocktionMVC/obj/Release/Package/PackageTmp/Scripts/MyLib/Data/userPrinter.js
+++ CocktionMVC/obj/Release/Package/PackageTmp/Scripts/MyLib/Data/userPrinter.js
@@ -11,14 +11,27 @@ function addUser(name, surname, id ,photoPath, rating, eggs, auctionsAmount, isF
             "<div class=\"userInfo\"><div class=\"row\"><div class=\"col-md-4\"> <img src=\"" + photoPath + "\"class=\"img-thumbnail\">" +
             "</div><div class=\"col-md-8\"> <p><b><a href=\"http://cocktion.com/Users/User/"+id+"\">" + name + ' ' + surname + "</a></b></p><p><b>Рейтинг:</b>" + rating +
             "</p> <p><b>Яйца:</b>" + eggs + "</p>" + "<p><b>Аукционов: </b>" + auctionsAmount + "</p>" +
-            "</div> </div> </div><br/>"
+            "<div class=\"col-md-4\"><p><button class=\"btn btn-default\">В друзья</button></p><p><button class=\"btn btn-default\">Предложить аукцион</button></p></div></div> </div> </div><br/>"
         );
     } else {
         $("#usersHere").append(
             "<div class=\"userInfo\"><div class=\"row\"><div class=\"col-md-4\"> <img src=\"" + photoPath + "\"class=\"img-thumbnail\">" +
             "</div><div class=\"col-md-8\"> <p><b><a href=\"http://cocktion.com/Users/User/" + id + "\">" + name + ' ' + surname + "</a></b></p><p><b>Рейтинг:</b>" + rating +
             "</p> <p><b>Яйца:</b>" + eggs + "</p>" + "<p><b>Аукционов: </b>" + auctionsAmount + "</p>" +
-            "</div> </div> </div>"
+            "<div class=\"col-md-4\"><p><button class=\"btn btn-default\">В друзья</button></p><p><button class=\"btn btn-default\">Предложить аукцион</button></p></div></div> </div> </div>"
         );
+    }
+}
+
+
+//проверяет чекбоксы ф чильтрах
+function sexCheckboxCheckedHandler(checkId) {
+    switch (checkId.id) {
+        case 'male':
+            $("#female").prop('checked', false);
+            break;
+        case 'female':
+            $("#male").prop('checked', false);
+            break;
     }
 }
