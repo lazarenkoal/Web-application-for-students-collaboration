096ee52a0513701a6da4d7e8f85b6ff0efa88b40
sanalazar <lazarenko.ale@gmail.com>
2015-04-22 17:25:57 +0300

    js hub file for realtime updates

---
+++ CocktionMVC/obj/Release/Package/PackageTmp/Scripts/MyLib/Auction/Index/hubConnection.js
@@ -0,0 +1,12 @@
+﻿//Коннектится к хабу и добавляет свеженькие аукциончики в список
+$(function () {
+    var adder = $.connection.auctionListHub;
+    adder.client.addAuctionToTheList = function (name, description, category, time, photoPath, id) {
+        addCellToTheGrid('auctionPanel', name, description,
+                   time, 'http://cocktion.com/Images/Thumbnails/' + photoPath,
+                   'http://cocktion.com/Auction/CurrentAuction/' + id);
+    }
+    $.connection.hub.start().done(function () {
+    });
+});
+
